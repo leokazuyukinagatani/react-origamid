b2535b3074aed49434b44c8e632668230085ca1f
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-11-22 15:11:28 -0300

    feat: aula0207

---
+++ aula02/src/App.tsx
@@ -0,0 +1,51 @@
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+
+function App() {
+  const dados = luana;
+
+  const compras = dados.compras.map((compra) =>
+    Number(compra.preco.replace("R$ ", ""))
+  );
+  const result = compras.reduce((acc, item) => acc + item);
+  return (
+    <>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{" "}
+        <span style={{ color: dados.ativa ? "green" : "red" }}>
+          {dados.ativa ? "Ativa" : "Inativa"}
+        </span>
+      </p>
+      <p>Total gasto: {result}</p>
+      {result > 10000 && <p>Você esta gastando muito</p>}
+    </>
+  );
+}
+
+export default App;
